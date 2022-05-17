443930ed0bf35db19c083e3a2b1177ab40f7b8d2
cabrerac <christiancabrera89@gmail.com>
2022-05-17 16:29:53 +0100

    simple microservice working locally

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ square_service = {'name': 'doa-composition-square-service', 'imageUrl': '', 'por
                   'path': '/doa_service_composition/add', 'priority': 2, 'count': 1}
 services = [add_service, square_service]
 
-deploy_to_aws.deploy_services('doa-resources-template.yml','doa-service-template.yml',services)
+#deploy_to_aws.deploy_services('doa-resources-template.yml','doa-service-template.yml',services)
 
-#base_url = 'http://doa-composition-load-balancer-692124022.eu-west-2.elb.amazonaws.com:80'
+base_url = 'http://doa-composition-load-balancer-692124022.eu-west-2.elb.amazonaws.com:80'
 
-#url = base_url + '/doa_sc/get_data'
-#response = requests.get(url)
-#print(response.content)
+url = base_url + '/doa_sc/get_data'
+response = requests.get(url)
+print(response.content)
 
 #url = base_url + '/doa_service_composition/square'
 #parameters = {'s1': 6, 's2': 6}
