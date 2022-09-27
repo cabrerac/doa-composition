a5b016e21caf4ecdeab87c2199415da386e0db58
cabrerac <christiancabrera89@gmail.com>
2022-09-27 16:52:41 +0100

    new results

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def doa_composition(request, n, le):
                    'execution_time': 0, 'total_time': 0, 'messages_size': 0, 'input_size': sys.getsizeof(str(request))}
     producer.publish(topic, message_dict)
     metrics[req_id] = measurement
-    request_outputs[req_id]['outputs'] = request['outputs']
+    request_outputs[req_id] = {'outputs': request['outputs']}
 
 
 # conversation based composition approach
