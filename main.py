ca3d2fb5184cdbc808c9c42bccf68468d1545a9f
cabrerac <christiancabrera89@gmail.com>
2022-09-29 11:47:14 +0100

    updating async template

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ def main(parameters_file):
     if 'doa' in approaches:
         print('- Deploying asynchronous services')
         services_async = generator.get_services('async', experiment, created_services)
-        #deploy_to_aws.deploy_services('templates/doa-service-template.yml', services_async)
+        deploy_to_aws.deploy_services('templates/doa-service-template.yml', services_async)
         rabbit_doa_consumer()
     if 'conversation' in approaches or 'planning' in approaches:
         print('- Deploying synchronous services')
@@ -171,7 +171,7 @@ def main(parameters_file):
 
     # Running experiments
     print('4. Running experiments...')
-    """for services_number in services:
+    for services_number in services:
         if 'conversation' in approaches or 'planning' in approaches:
             print('- Registering services: ' + str(services_number))
             registry_services = generator.get_services_to_register('sync', experiment, services_number, created_services)
@@ -199,11 +199,11 @@ def main(parameters_file):
                     i = i + 1
                     time.sleep(2)
         if 'conversation' in approaches or 'planning' in approaches:
-            data_access.remove_services()"""
+            data_access.remove_services()
     # removing services from AWS
     print('7. Removing services...')
-    if 'doa' in approaches:
-        deploy_to_aws.remove_services(services_async)
+    #if 'doa' in approaches:
+    #    deploy_to_aws.remove_services(services_async)
     if 'conversation' in approaches or 'planning' in approaches:
         deploy_to_aws.remove_services(services_sync)
     # plotting results
